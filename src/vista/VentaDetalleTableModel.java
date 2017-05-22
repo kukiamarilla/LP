4ff3612899a3f0f365c6130c53db27d9473754ae
Larrea_Barrios <Larrea_Barrios>
2017-05-22 15:04:20 -0400

    Vistas

---
+++ src/vista/VentaDetalleTableModel.java
@@ -0,0 +1,297 @@
+
+package vista;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import modelo.Productos;
+import modelo.DetallesVentas;
+/**
+ *
+ * @author gustavo
+ */
+public class VentaDetalleTableModel extends AbstractTableModel{
+
+    /**
+     * Objeto Lista que almacena los Detalles de Compra a mostrar en
+     * la tabla
+     */
+    private List<DetallesVentas> datos = new ArrayList<DetallesVentas>();
+
+    /**
+     * Retorna la lista que almacena los detalles de compra
+     */
+    public List<DetallesVentas> getDatos() {
+        return datos;
+    }
+
+    /**
+     * Establece los detalles de compra a mostrar por este modelo
+     */
+    public void setDatos(List<DetallesVentas> datos) {
+        this.datos = datos;
+        this.fireTableDataChanged();
+    }
+
+    /**
+     * Retorna los nombres de la columnas definidas por el modelo. El modelo
+     * consta de 5 columnas. Si se pide una columna fuera del rango se lanza
+     * la excepcion no comprobada IndexOfBoundsException
+     */
+    @Override
+    public String getColumnName(int col) {
+        switch (col) {
+            case 0:
+                return "Cod. Producto";
+            case 1:
+                return "Descripción";
+            case 2:
+                return "Precio Un.";
+            case 3:
+                return "Cantidad";
+            case 4:
+                return "SubTotal";
+        }
+
+        throw new IndexOutOfBoundsException("No existe la columna " + col);
+    }
+
+    /**
+     * Retorna la clase a la que pertenecen los datos en la columna
+     * especificada por el parámetro
+     */
+    @Override
+    public Class getColumnClass(int col) {
+        switch (col) {
+            case 0:
+            case 1:
+                return String.class;
+            case 2:
+            case 4:
+                return Double.class;
+            case 3:
+                return Integer.class;
+        }
+
+        throw new IndexOutOfBoundsException("No existe la columna " + col);
+    }
+
+    /**
+     * Retorna el número de columnas del modelo
+     */
+    @Override
+    public int getColumnCount() {
+        return 5;
+    }
+
+    /**
+     * Retorna el valor almacenado en la celda de la de tabla especificada
+     * por los parámetros.<br>
+     * En el módelo de datos: la columna 0 indica el código del Producto,
+     * la columna 1 la descrpción del producto,
+     * la columna 2 el precio unitario,
+     * la columna 3 la cantidad a vender
+     * y la columna 4 el subtotal del detalle.
+     */
+    @Override
+    public Object getValueAt(int fila, int columna) {
+        if (fila < 0 || fila >= datos.size()) {
+            throw new IndexOutOfBoundsException("No existe la fila " + fila);
+        }
+
+        if (columna < 0 || columna > 4) {
+            throw new IndexOutOfBoundsException("No existe la columna " + columna);
+        }
+
+        DetallesVentas vd = datos.get(fila);
+
+        switch (columna) {
+            case 0:
+                if (vd.getProductos() == null) {
+                    return null;
+                }
+                return vd.getProductos().getId();
+            case 1:
+                if (vd.getProductos() == null) {
+                    return null;
+                }
+                return vd.getProductos().getDescripcion();
+            case 2:
+                if (vd.getPrecio() == null && vd.getProductos() != null) {
+                        vd.setPrecio(vd.getProductos().getPrecio());
+                }
+                return vd.getPrecio();
+            case 3:
+                if (vd.getCantidad() == null) {
+                        vd.setCantidad(1);
+                }
+                return vd.getCantidad();
+            default:
+                if (vd.getCantidad() == null || vd.getPrecio() == null) {
+                    return null;
+                }
+                return vd.getCantidad() * vd.getPrecio();
+        }
+    }
+
+    /**
+     * Establece el valor para la celda especificada por los párametros.
+     * Sólo pueden establecerce valores para las columnas 0, 2 y 3. Si intenta
+     * establecerce un valor en las otros columnas se lanza la excepción
+     * IllegalArgumentException. Las columnas 1 a 4 son de solo lectura.
+     * Si el valor a establecer no es apropiado,
+     * este modelo no acepta el valor.<br>
+     * <br>
+     * En la columna 0 solo se admiten objetos Producto que no estén en
+     * este módelo, asi que no existen dos detalles para un mismo
+     * Producto.<br>
+     * <br>
+     * En la columna 2 solo se admiten valores enteros mayores a cero o el valor
+     * null. <br>
+     * <br>
+     * En la columna 3 solo se admiten valores doubles mayores a cero o el valor
+     * null. <br>
+     * <br>
+     * Este método lanza el evento de celda actualizada
+     */
+    @Override
+    public void setValueAt(Object valor, int fila, int columna) {
+        if (fila < 0 || fila >= datos.size()) {
+            throw new IndexOutOfBoundsException("No existe la fila " + fila);
+        }
+
+        if (columna < 0 || columna > 4) {
+            throw new IndexOutOfBoundsException("No existe la columna " + columna);
+        }
+
+        DetallesVentas vd = datos.get(fila);
+        switch (columna) {
+            case 0:
+                if (valor != null && !(valor instanceof Productos)) {
+                    throw new IllegalArgumentException("El parámetro para la "
+                            + "columna " + columna + " deber ser un Producto");
+                }
+                // no se acepta el cambio si existe otro detalle para el
+                // Producto que se quiere establecer
+                for (DetallesVentas fd2 : datos) {
+                    if (fd2 != vd && fd2.getProductos().getId().equals(
+                            ((Productos) valor).getId())) {
+                        return;
+                    }
+                }
+                vd.setProductos((Productos) valor);
+                // se informa del cambio en todas las columnas generadas
+                // por el objeto Producto (id, descripcion, precio)
+                vd.setPrecio(null);
+                vd.setCantidad(null);
+                fireTableCellUpdated(fila, 0);
+                fireTableCellUpdated(fila, 1);
+                fireTableCellUpdated(fila, 2);
+                fireTableCellUpdated(fila, 3);
+                fireTableCellUpdated(fila, 4);
+                return;
+            case 1:
+            case 4:
+                throw new IllegalArgumentException("La columna " + columna
+                        + " no puede ser editada");
+            case 2:
+                if (valor != null && !(valor instanceof Double)) {
+                    throw new IllegalArgumentException("El parámetro para la "
+                            + "columna " + columna + " deber ser Double");
+                }
+                vd.setPrecio((Double) valor);
+                fireTableCellUpdated(fila, 2);
+                fireTableCellUpdated(fila, 4);
+                return;
+            case 3:
+                if (valor != null && !(valor instanceof Integer)) {
+                    throw new IllegalArgumentException("El parámetro para la "
+                            + "columna " + columna + " deber ser Integer");
+                }
+                vd.setCantidad((Integer) valor);
+                fireTableCellUpdated(fila, 3);
+                fireTableCellUpdated(fila, 4);
+                return;
+        }
+    }
+
+    /**
+     * Retorna el numero de filas.
+     */
+    @Override
+    public int getRowCount() {
+        if (datos == null) {
+            return 0;
+        }
+        return datos.size();
+    }
+
+    /**
+     * Retorna un valor boolean que indica si una celda del modelo es editable.
+     * Solo las celdas en las columnas 0, 2 y 3 son editables, por lo que Swing
+     * ofrecera un mecanismo de edición adecuado para las mismas según su clase.
+     */
+    @Override
+    public boolean isCellEditable(int fila, int col) {
+        if (fila < 0 || fila >= datos.size()) {
+            throw new IndexOutOfBoundsException("No existe la fila " + fila);
+        }
+        if (col < 0 || col > 4) {
+            throw new IndexOutOfBoundsException("No existe la columna " + col);
+        }
+        if (col == 0 || col == 2 || col == 3) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Agrega el objeto detalle indicada al final de la lista en este modelo.
+     * Este método lanza el evento de fila insertada en el modelo.
+     */
+    public void agregarDetalle(DetallesVentas vd) {
+        if (datos == null) {
+            datos = new ArrayList<DetallesVentas>();
+        }
+        datos.add(vd);
+        this.fireTableRowsInserted(datos.size() - 1, datos.size() - 1);
+    }
+
+    /**
+     * Remueve la fila indicada del modelo. Este método lanza el evento de fila
+     * removida en el modelo.
+     */
+    public void removerDetalle(int fila) {
+        if (fila < 0 || fila > datos.size()) {
+            throw new IndexOutOfBoundsException("No existe la fila " + fila);
+        }
+        datos.remove(fila);
+        this.fireTableRowsDeleted(fila, fila);
+    }
+
+    /**
+     * Remueve el objeto detalle del modelo. Este método lanza el evento de fila
+     * removida en el modelo.
+     */
+    public void removerDetalle(DetallesVentas vd) {
+        if (datos.contains(vd)) {
+            int fila = datos.indexOf(vd);
+            datos.remove(fila);
+            this.fireTableRowsDeleted(fila, fila);
+        }
+    }
+
+    /**
+     * Actualiza la fila indicada con el detalle recibido como parámetro. Este
+     * método lanza el evento de fila actualizada sobre el modelo.
+     */
+    public void actualizarDetalle(int fila, DetallesVentas vd) {
+        if (fila < 0 || fila > datos.size()) {
+            throw new IndexOutOfBoundsException("No existe la fila " + fila);
+        }
+        datos.remove(fila);
+        datos.add(fila, vd);
+        this.fireTableRowsUpdated(fila, fila);
+    }
+
+}
---
+++ src/vista/VentanaPrincipal.java
@@ -0,0 +1,351 @@
+/*
+ * VentanaPrincipal.java
+ *
+ * Created on 25 de mayo de 2008, 11:05 AM
+ */
+package py.una.pol.lp2.compraVenta.vista;
+
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.event.InternalFrameAdapter;
+import javax.swing.event.InternalFrameEvent;
+
+/**
+ *
+ * @author  gustavo
+ */
+public class VentanaPrincipal extends JFrame {
+
+    private VentanaAbmProducto vap = null;
+    private VentanaAbmProveedor vape = null;
+    private VentanaListadoProducto vlp = null;
+    private VentanaListadoProveedores vlpr = null;
+    private VentanaCargaCompra vcc = null;
+    private VentanaListadoCompras vlcp = null;
+
+    public VentanaPrincipal() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panel = new javax.swing.JDesktopPane();
+        menuBar = new javax.swing.JMenuBar();
+        menuArchivo = new javax.swing.JMenu();
+        itemSalir = new javax.swing.JMenuItem();
+        menuAbm = new javax.swing.JMenu();
+        abmProducto = new javax.swing.JMenuItem();
+        proveedor = new javax.swing.JMenuItem();
+        abmCliente = new javax.swing.JMenuItem();
+        menuCargaDatos = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        CargaCompa = new javax.swing.JMenuItem();
+        menuListados = new javax.swing.JMenu();
+        listadoProducto = new javax.swing.JMenuItem();
+        listadoVenta = new javax.swing.JMenuItem();
+        listadoProveedores = new javax.swing.JMenuItem();
+        listadoClientes = new javax.swing.JMenuItem();
+        listarCompras = new javax.swing.JMenuItem();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Aplicación Fabri Ventas");
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.LINE_AXIS));
+
+        panel.setBackground(new java.awt.Color(204, 204, 204));
+        getContentPane().add(panel);
+
+        menuArchivo.setText("Archivo");
+
+        itemSalir.setText("Salir");
+        itemSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itemSalirActionPerformed(evt);
+            }
+        });
+        menuArchivo.add(itemSalir);
+
+        menuBar.add(menuArchivo);
+
+        menuAbm.setText("ABM");
+
+        abmProducto.setText("Producto");
+        abmProducto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                abmProductoActionPerformed(evt);
+            }
+        });
+        menuAbm.add(abmProducto);
+
+        proveedor.setText("Proveedor");
+        proveedor.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                proveedorActionPerformed(evt);
+            }
+        });
+        menuAbm.add(proveedor);
+
+        abmCliente.setText("Cliente");
+        abmCliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                abmClienteActionPerformed(evt);
+            }
+        });
+        menuAbm.add(abmCliente);
+
+        menuBar.add(menuAbm);
+
+        menuCargaDatos.setText("Carga Datos");
+
+        jMenuItem1.setText("Venta");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        menuCargaDatos.add(jMenuItem1);
+
+        CargaCompa.setText("Compra");
+        CargaCompa.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                CargaCompaActionPerformed(evt);
+            }
+        });
+        menuCargaDatos.add(CargaCompa);
+
+        menuBar.add(menuCargaDatos);
+
+        menuListados.setText("Listados");
+
+        listadoProducto.setText("Listado Producto");
+        listadoProducto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                listadoProductoActionPerformed(evt);
+            }
+        });
+        menuListados.add(listadoProducto);
+
+        listadoVenta.setText("Listado Venta");
+        listadoVenta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                listadoVentaActionPerformed(evt);
+            }
+        });
+        menuListados.add(listadoVenta);
+
+        listadoProveedores.setText("Listado Proveedores");
+        listadoProveedores.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                listadoProveedoresActionPerformed(evt);
+            }
+        });
+        menuListados.add(listadoProveedores);
+
+        listadoClientes.setText("Listado Clientes");
+        listadoClientes.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                listadoClientesActionPerformed(evt);
+            }
+        });
+        menuListados.add(listadoClientes);
+
+        listarCompras.setText("Listar Compras");
+        listarCompras.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                listarComprasActionPerformed(evt);
+            }
+        });
+        listarCompras.addMenuDragMouseListener(new javax.swing.event.MenuDragMouseListener() {
+            public void menuDragMouseDragged(javax.swing.event.MenuDragMouseEvent evt) {
+                listarComprasMenuDragMouseDragged(evt);
+            }
+            public void menuDragMouseEntered(javax.swing.event.MenuDragMouseEvent evt) {
+            }
+            public void menuDragMouseExited(javax.swing.event.MenuDragMouseEvent evt) {
+            }
+            public void menuDragMouseReleased(javax.swing.event.MenuDragMouseEvent evt) {
+            }
+        });
+        menuListados.add(listarCompras);
+
+        menuBar.add(menuListados);
+
+        setJMenuBar(menuBar);
+    }// </editor-fold>//GEN-END:initComponents
+
+private void itemSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemSalirActionPerformed
+    System.exit(0);
+}//GEN-LAST:event_itemSalirActionPerformed
+
+private void abmProductoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_abmProductoActionPerformed
+    if (vap == null) {
+        vap = new VentanaAbmProducto();
+        panel.add(vap);
+        vap.addInternalFrameListener(new InternalFrameAdapter() {
+
+            @Override
+            public void internalFrameClosed(InternalFrameEvent e) {
+                vap = null;
+            }
+        });
+        vap.setVisible(true);
+    } else { // si la ventana esta abierta, le damos el foco.
+        try {
+            vap.setSelected(true);
+        } catch (Exception ignorada) {
+        }
+    }
+}//GEN-LAST:event_abmProductoActionPerformed
+
+private void listadoProductoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_listadoProductoActionPerformed
+    if (vlp == null) {
+        vlp = new VentanaListadoProducto();
+        panel.add(vlp);
+        vlp.addInternalFrameListener(new InternalFrameAdapter() {
+
+            @Override
+            public void internalFrameClosed(InternalFrameEvent e) {
+                vlp = null;
+            }
+        });
+        vlp.setVisible(true);
+    } else { // si la ventana esta abierta, le damos el foco.
+        try {
+            vlp.setSelected(true);
+        } catch (Exception ignorada) {
+        }
+    }
+}//GEN-LAST:event_listadoProductoActionPerformed
+
+private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+    
+}//GEN-LAST:event_jMenuItem1ActionPerformed
+
+private void listadoVentaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_listadoVentaActionPerformed
+    
+}//GEN-LAST:event_listadoVentaActionPerformed
+
+private void proveedorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_proveedorActionPerformed
+    // TODO add your handling code here:
+
+    if (vape == null) {
+        vape = new VentanaAbmProveedor();
+        panel.add(vape);
+        vape.addInternalFrameListener(new InternalFrameAdapter() {
+
+            @Override
+            public void internalFrameClosed(InternalFrameEvent e) {
+                vape = null;
+            }
+        });
+        vape.setVisible(true);
+    } else { // si la ventana esta abierta, le damos el foco.
+        try {
+            vape.setSelected(true);
+        } catch (Exception ignorada) {
+        }
+    }
+}//GEN-LAST:event_proveedorActionPerformed
+
+private void abmClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_abmClienteActionPerformed
+    
+}//GEN-LAST:event_abmClienteActionPerformed
+
+private void CargaCompaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CargaCompaActionPerformed
+    // TODO add your handling code here:
+
+    if (vcc == null) {
+        vcc = new VentanaCargaCompra();
+        panel.add(vcc);
+        vcc.addInternalFrameListener(new InternalFrameAdapter() {
+
+            @Override
+            public void internalFrameClosed(InternalFrameEvent e) {
+                vcc = null;
+            }
+        });
+        vcc.setVisible(true);
+    } else { // si la ventana esta abierta, le damos el foco.
+        try {
+            vcc.setSelected(true);
+        } catch (Exception ignorada) {
+        }
+    }
+}//GEN-LAST:event_CargaCompaActionPerformed
+
+private void listadoProveedoresActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_listadoProveedoresActionPerformed
+    // TODO add your handling code here:
+
+    if (vlpr == null) {
+        vlpr = new VentanaListadoProveedores();
+        panel.add(vlpr);
+        vlpr.addInternalFrameListener(new InternalFrameAdapter() {
+
+            @Override
+            public void internalFrameClosed(InternalFrameEvent e) {
+                vlpr = null;
+            }
+        });
+        vlpr.setVisible(true);
+    } else { // si la ventana esta abierta, le damos el foco.
+        try {
+            vlpr.setSelected(true);
+        } catch (Exception ignorada) {
+        }
+    }
+}//GEN-LAST:event_listadoProveedoresActionPerformed
+
+private void listadoClientesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_listadoClientesActionPerformed
+    
+}//GEN-LAST:event_listadoClientesActionPerformed
+
+private void listarComprasMenuDragMouseDragged(javax.swing.event.MenuDragMouseEvent evt) {//GEN-FIRST:event_listarComprasMenuDragMouseDragged
+    // TODO add your handling code here:
+
+}//GEN-LAST:event_listarComprasMenuDragMouseDragged
+
+private void listarComprasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_listarComprasActionPerformed
+    // TODO add your handling code here:
+
+
+    if (vlcp == null) {
+        vlcp = new VentanaListadoCompras();
+        panel.add(vlcp);
+        vlcp.addInternalFrameListener(new InternalFrameAdapter() {
+
+            @Override
+            public void internalFrameClosed(InternalFrameEvent e) {
+                vlcp = null;
+            }
+        });
+        vlcp.setVisible(true);
+    } else { // si la ventana esta abierta, le damos el foco.
+        try {
+            vlcp.setSelected(true);
+        } catch (Exception ignorada) {
+        }
+    }
+}//GEN-LAST:event_listarComprasActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem CargaCompa;
+    private javax.swing.JMenuItem abmCliente;
+    private javax.swing.JMenuItem abmProducto;
+    private javax.swing.JMenuItem itemSalir;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem listadoClientes;
+    private javax.swing.JMenuItem listadoProducto;
+    private javax.swing.JMenuItem listadoProveedores;
+    private javax.swing.JMenuItem listadoVenta;
+    private javax.swing.JMenuItem listarCompras;
+    private javax.swing.JMenu menuAbm;
+    private javax.swing.JMenu menuArchivo;
+    private javax.swing.JMenuBar menuBar;
+    private javax.swing.JMenu menuCargaDatos;
+    private javax.swing.JMenu menuListados;
+    protected javax.swing.JDesktopPane panel;
+    private javax.swing.JMenuItem proveedor;
+    // End of variables declaration//GEN-END:variables
+}
